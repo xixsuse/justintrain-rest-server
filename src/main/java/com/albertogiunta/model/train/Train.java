2ec8ee358cb2e60b86a47786b8f1cc515ad694fe
Alberto Giunta <alb.giunta@gmail.com>
2016-11-07 16:45:52 +0100

    Fix to train header when train of change is requested

--- src/main/java/com/albertogiunta/model/train/Train.java
+++ src/main/java/com/albertogiunta/model/train/Train.java
@@ -19,26 +19,26 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @SuppressWarnings("unused")
 public class Train {
-
+    
     private String trainCategory;
     private String trainId;
-
+    
     private String trainDepartureStationId;
     private String trainDepartureStationName;
 //    private String departurePlatform;
     
     private String trainArrivalStationId;
     private String trainArrivalStationName;
-
+    
     private Integer timeDifference; //TODO vedi se calcolarlo anche a mano
     private Integer progress;
-
+    
     private String lastSeenStationName;
     private String lastSeenTimeReadable; // in HH:mm
-
+    
     private List<Stop> stops = new ArrayList<>();
     private String cancelledStopsInfo;
-
+    
     private Integer firstClassOrientationCode;
     private Integer trainStatusCode;
     private Boolean isDeparted;
@@ -47,25 +47,25 @@ public class Train {
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* TRAIN CATEGORY */
     @JsonGetter(JFIELD.TRAIN_CATEGORY)
     public String getTrainCategory() {
         return trainCategory;
     }
-
+    
     @JsonSetter(TFIELD.TRAIN_CATEGORY_FOR_TRAIN)
     @JsonDeserialize(using = TrainCategoryDeserializer.class)
     public void setTrainCategory(String trainCategory) {
         this.trainCategory = trainCategory;
     }
-
+    
     /* TRAIN NUMBER */
     @JsonGetter(JFIELD.TRAIN_ID)
     public String getTrainId() {
         return trainId;
     }
-
+    
     @JsonProperty(TFIELD.TRAIN_ID)
     public void setTrainId(String trainId) {
         this.trainId = trainId;
@@ -73,24 +73,24 @@ public class Train {
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* DEPARTURE STATION ID */
     @JsonGetter(JFIELD.TRAIN_DEPARTURE_STATION_ID)
     public String getTrainDepartureStationId() {
         return trainDepartureStationId;
     }
-
+    
     @JsonSetter(TFIELD.DEPARTURE_STATION_ID)
     public void setTrainDepartureStationId(String trainDepartureStationId) {
         this.trainDepartureStationId = trainDepartureStationId;
     }
-
+    
     /* DEPARTURE STATION NAME */
     @JsonGetter(JFIELD.TRAIN_DEPARTURE_STATION_NAME)
     public String getTrainDepartureStationName() {
         return trainDepartureStationName;
     }
-
+    
     @JsonSetter(TFIELD.DEPARTURE_STATION_NAME)
     public void setTrainDepartureStationName(String trainDepartureStationName) {
         this.trainDepartureStationName = trainDepartureStationName;
@@ -109,24 +109,24 @@ public class Train {
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* ARRIVAL STATION ID */
     @JsonGetter(JFIELD.TRAIN_ARRIVAL_STATION_ID)
     public String getTrainArrivalStationId() {
         return trainArrivalStationId;
     }
-
+    
     @JsonSetter(TFIELD.ARRIVAL_STATION_ID)
     public void setTrainArrivalStationId(String trainArrivalStationId) {
         this.trainArrivalStationId = trainArrivalStationId;
     }
-
+    
     /* ARRIVAL STATION NAME*/
     @JsonGetter(JFIELD.TRAIN_ARRIVAL_STATION_NAME)
     public String getTrainArrivalStationName() {
         return trainArrivalStationName;
     }
-
+    
     @JsonSetter(TFIELD.ARRIVAL_STATION_NAME)
     public void setTrainArrivalStationName(String trainArrivalStationName) {
         this.trainArrivalStationName = trainArrivalStationName;
@@ -134,50 +134,50 @@ public class Train {
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* TIME DIFFERENCE */
     @JsonGetter(JFIELD.TIME_DIFFERENCE)
     public Integer getTimeDifference() {
         return timeDifference;
     }
-
+    
     @JsonSetter(TFIELD.TIME_DIFFERENCE_FOR_TRAIN)
     public void setTimeDifference(Integer timeDifference) {
         this.timeDifference = timeDifference;
         doAsLatest();
     }
-
+    
     /* PROGRESS */
     @JsonGetter(JFIELD.PROGRESS)
     public Integer getProgress() {
         return progress;
     }
-
+    
     public void setProgress(Integer progress) {
         this.progress = progress;
     }
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* LAST SEEN STATION */
     @JsonGetter(JFIELD.LAST_SEEN_STATION_NAME)
     public String getLastSeenStationName() {
         return lastSeenStationName;
     }
-
+    
     @JsonSetter(TFIELD.LAST_SEEN_STATION_NAME)
     @JsonDeserialize(using = LastSeenSerializer.class)
     public void setLastSeenStationName(String lastSeenStationName) {
         this.lastSeenStationName = lastSeenStationName;
     }
-
+    
     /* LAST SEEN TIME */
     @JsonGetter(JFIELD.LAST_SEEN_TIME_READABLE)
     public String getLastSeenTimeReadable() {
         return lastSeenTimeReadable;
     }
-
+    
     @JsonSetter(TFIELD.LAST_SEEN_TIME_READABLE)
     @JsonDeserialize(using = LastSeenSerializer.class)
     public void setLastSeenTimeReadable(String lastSeenTime) {
@@ -186,24 +186,24 @@ public class Train {
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* STATION ARRAY */
     @JsonGetter(JFIELD.STOPS)
     public List<Stop> getStops() {
         return stops;
     }
-
+    
     @JsonSetter(TFIELD.STOPS)
     public void setStops(List<Stop> stops) {
         this.stops = stops;
     }
-
+    
     /* CANCELLED STOPS INFO */
     @JsonGetter(JFIELD.CANCELLED_STOPS_INFO)
     public String getCancelledStopsInfo() {
         return cancelledStopsInfo;
     }
-
+    
     @JsonSetter(TFIELD.CANCELLED_STOPS_INFO)
     public void setCancelledStopsInfo(String cancelledStopsInfo) {
         this.cancelledStopsInfo = cancelledStopsInfo;
@@ -211,54 +211,54 @@ public class Train {
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     /* FIRST CLASS ORIENTATION */
     @JsonGetter(JFIELD.FIRST_CLASS_ORIENTATION)
     public Integer getFirstClassOrientationCode() {
         return firstClassOrientationCode;
     }
-
+    
     @JsonSetter(TFIELD.FIRST_CLASS_ORIENTATION)
     @JsonDeserialize(using = TrainOrientationDeserializer.class)
     public void setFirstClassOrientationCode(Integer firstClassOrientationCode) {
         this.firstClassOrientationCode = firstClassOrientationCode;
     }
-
+    
     /* TRAIN STATUS COE */
     @JsonGetter(JFIELD.TRAIN_STATUS_CODE)
     public Integer getTrainStatusCode() {
         return this.trainStatusCode;
     }
-
+    
     @JsonSetter(TFIELD.TRAIN_STATUS_CODE)
     @JsonDeserialize(using = TrainStatusCodeDeserializer.class)
     public void setTrainStatusCode(Integer trainStatusMessage) {
         this.trainStatusCode = trainStatusMessage;
     }
-
+    
     /* IS DEPARTED*/
     @JsonGetter(value = JFIELD.IS_DEPARTED)
     public Boolean isDeparted() {
         return isDeparted;
     }
-
+    
     public void setDeparted(Boolean departed) {
         isDeparted = departed;
     }
-
+    
     /* ARRIVED TO DESTINATION */
     @JsonGetter(value = JFIELD.IS_ARRIVED_TO_DESTINATION)
     public Boolean isArrivedToDestination() {
         return isArrivedToDestination;
     }
-
+    
     public void setArrivedToDestination(Boolean arrivedToDestination) {
         isArrivedToDestination = arrivedToDestination;
     }
 
     /*--------------------------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------------------------*/
-
+    
     private void doAsLatest() {
 //        setPlannedDeparturePlatform();
         if (stops.size() > 0) {
@@ -267,7 +267,7 @@ public class Train {
             setProgress(setProgressIndicator(stops));
         }
     }
-
+    
     private Integer setProgressIndicator(List<Stop> oldArray) {
         List<Stop> visited = new LinkedList<>();
         long delta = 0L;
@@ -277,7 +277,7 @@ public class Train {
                 visited.add(s);
             }
         }
-
+        
         if (visited.size() <= 5) {
             for (Integer i = visited.size() - 2; i >= 0; i--) {
                 intermediateDelta = visited.get(i + 1).getTimeDifference() - visited.get(i).getTimeDifference();
@@ -297,7 +297,7 @@ public class Train {
             return JVALUE.CONSTANT;
         }
     }
-
+    
     public Stop getStopDataWithStationName(String stationName) {
         for (Stop stop : stops) {
             if (stop.getStationName().equalsIgnoreCase(stationName)) {
@@ -306,17 +306,17 @@ public class Train {
         }
         return null;
     }
-
+    
     public Stop getStopDataWithStationId(String stationId) {
         for (Stop stop : stops) {
             if (stop.getStationId().replaceAll("(S|N)0+|(S|N)", "").equalsIgnoreCase(stationId)) {
                 return stop;
             }
         }
-        return null;
+        return getStopDataWithStationName(stationId);
     }
-
-
+    
+    
     @Override
     public String toString() {
         return "Train{" +
